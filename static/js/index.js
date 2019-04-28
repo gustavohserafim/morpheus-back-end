67b6a0c1024ac7af9ddc70e5c739e935231b9f22
DESKTOP-BD8ELCJ\Gustavo <tioogu@gmail.com>
2019-04-28 16:44:15 -0300

    feat: logim frontend

---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+function checkLoggedUser() {
+    var token = $.cookie('access_token_cookie');
+
+    if (token === undefined) {
+        window.location = '/static/login.html';
+    }
+}
+
+function getHome() {
+
+    $.ajax({
+        type: "GET",
+        url: "/api/company",
+        //dataType: "json",
+        //contentType: 'application/json',
+        success: function () {
+            $('#login_form').hide();
+            $('#logged').show();
+        }
+    });
+}
+
+$(document).ready(function () {
+   checkLoggedUser();
+
+
+});
