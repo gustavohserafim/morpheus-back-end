9962e8c605f2d148a92a65111a97e6e378a864cc
gustavohserafim <tioogu@gmail.com>
2019-06-04 20:53:45 -0300

    fix: path

--- static/js/util.js
+++ static/js/util.js
@@ -2,7 +2,7 @@ function checkLoggedUser() {
     var token = $.cookie('access_token_cookie');
 
     if (token === undefined) {
-        window.location = '/login';
+        window.location = '/static/login.html';
     }
 }
 
