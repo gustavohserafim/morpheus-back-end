ed6dc900745624636651438282e6d6623eb3a841
IGuedes <icarogsantos@gmail.com>
2019-06-06 23:20:53 -0300

    fix: nightmode test

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,19 @@
 function getStylesheet() {
       var currentTime = new Date().getHours();
       if (0 <= currentTime&&currentTime < 5) {
-       document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='night.css' type='text/css'>");
       }
       if (5 <= currentTime&&currentTime < 11) {
-       document.write("<link rel='stylesheet' href='main.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='day.css' type='text/css'>");
       }
       if (11 <= currentTime&&currentTime < 16) {
-       document.write("<link rel='stylesheet' href='main.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='day.css' type='text/css'>");
       }
       if (16 <= currentTime&&currentTime < 19) {
-       document.write("<link rel='stylesheet' href='main.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='day.css' type='text/css'>");
       }
       if (22 <= currentTime&&currentTime <= 24) {
-       document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='night.css' type='text/css'>");
       }
 }
 
