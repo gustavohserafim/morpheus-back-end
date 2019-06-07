51c22b47e84a93ac04bcb5eb8570f5b35500b091
IGuedes <icarogsantos@gmail.com>
2019-06-06 23:30:36 -0300

    fix: nightmode tests

--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,19 @@
 function getStylesheet() {
       var currentTime = new Date().getHours();
       if (0 <= currentTime&&currentTime < 5) {
-          $('#nightmode').html("<link rel='stylesheet' href='night.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='/css/night.css' type='text/css'>");
       }
       if (5 <= currentTime&&currentTime < 11) {
-          $('#nightmode').html("<link rel='stylesheet' href='day.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='/css/day.css' type='text/css'>");
       }
       if (11 <= currentTime&&currentTime < 16) {
-          $('#nightmode').html("<link rel='stylesheet' href='day.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='/css/day.css' type='text/css'>");
       }
       if (16 <= currentTime&&currentTime < 19) {
-          $('#nightmode').html("<link rel='stylesheet' href='day.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='/css/day.css' type='text/css'>");
       }
       if (22 <= currentTime&&currentTime <= 24) {
-          $('#nightmode').html("<link rel='stylesheet' href='night.css' type='text/css'>");
+          $('#nightmode').html("<link rel='stylesheet' href='/css/night.css' type='text/css'>");
       }
 }
 
